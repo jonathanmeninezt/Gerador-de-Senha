36844328a1af9293cea177c9920d7e4a31e0641d
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-03-08 10:56:27 -0300

    criação- projeto gerador de senha- function/generatePassworld

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function generatePassworld(){
+const chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]"
+
+let passworld = ""
+
+for (let i = 0; i < 8; i++){
+    const randomNumber = Math.floor(Math.random() * chars.length) // floor nivela tirando a virgula - random gera um nº aleatório
+    passworld += chars.substring(randomNumber, randomNumber + 1)  // faz um corte dentro da string chars
+    console.log(passworld)
+
+ }
+
+}
+
+generatePassworld()
