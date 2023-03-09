86cd5bb49cb303454537ba1de774e5ad86a2a2d8
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-03-09 14:46:59 -0300

    criação- section/box

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
+var passworldLength = 16
+const inputEl = document.querySelector("#passworld")
+
 function generatePassworld(){
 const chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]"
 
 let passworld = ""
 
-for (let i = 0; i < 8; i++){
+for (let i = 0; i < passworldLength; i++){
     const randomNumber = Math.floor(Math.random() * chars.length) // floor nivela tirando a virgula - random gera um nº aleatório
     passworld += chars.substring(randomNumber, randomNumber + 1)  // faz um corte dentro da string chars
     
   }
 
-  const inputEl = document.querySelector("#passworld")
+  
   inputEl.value = passworld
 
   }
 
+  function copy(){
+    navigator.clipboard.writeText(inputEl.value)  //clipboard - ação de copiar e colar
+  }
+
   const passworldLengthEl = document.querySelector('#passworld-length')
   passworldLengthEl.addEventListener("input", function() {
-    const passworldLength = passworldLengthEl.value
-    console.log(passworldLength)
+    passworldLength = passworldLengthEl.value
+    
+    generatePassworld()
 
   })
+
+  const copyButtonEl = document.querySelector('#copy')
+  copyButtonEl.addEventListener("click", copy)
     
 
   
