bf1af761927316662ff883e5415ad290d8b0379b
Jonathan Meninez <jonathan.meninezt@gmail.com>
2023-03-10 16:30:01 -0300

    criação- uppercasechars/number/symbol-checked

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 var passworldLength = 16
-const inputEl = document.querySelector("#passworld")
+const inputEl = document.querySelector("#password")
+const upperCaseCheckEl = document.querySelector("#uppercase-check")
+const numberCheckEl = document.querySelector("#number-check")
+const symbolCheckEl = document.querySelector("#symbol-check")
 
 function generatePassworld(){
-const chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]"
+let chars = "abcdefghjkmnpqrstuvwxyz"
+
+const uperCaseChars = "ABCDEFGHJKLMNPQRSTUVWXYZ"
+const numberChars = "123456789"
+const symbolChars = "?!@&*()[]"
+
+if (upperCaseCheckEl.checked){
+  chars += uperCaseChars
+
+}
+
+if (numberCheckEl.checked){
+  chars += numberChars
+}
+
+if (symbolCheckEl.checked){
+  chars += symbolChars
+}
+
+
 
 let passworld = ""
 
@@ -21,7 +43,7 @@ for (let i = 0; i < passworldLength; i++){
     navigator.clipboard.writeText(inputEl.value)  //clipboard - ação de copiar e colar
   }
 
-  const passworldLengthEl = document.querySelector('#passworld-length')
+  const passworldLengthEl = document.querySelector('#password-length')
   passworldLengthEl.addEventListener("input", function() {
     passworldLength = passworldLengthEl.value
     
@@ -29,12 +51,13 @@ for (let i = 0; i < passworldLength; i++){
 
   })
 
-  const copyButtonEl = document.querySelector('#copy')
+  upperCaseCheckEl.addEventListener("click",generatePassworld)
+  numberCheckEl.addEventListener("click", generatePassworld)
+  symbolCheckEl.addEventListener("click", generatePassworld)
+
+ 
+
+  const copyButtonEl = document.querySelector('#copy-2')
   copyButtonEl.addEventListener("click", copy)
     
-
-  
-
-
-
 generatePassworld()
